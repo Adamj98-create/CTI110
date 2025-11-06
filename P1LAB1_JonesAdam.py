db557bd44aa5584bca3a3c2dcfc36cb4c30a96c4
Adamj98-create <jonesa9837@student.faytechcc.edu>
2025-11-06 17:37:04 -0500

    Add files via upload

---
+++ P1LAB1_JonesAdam.py
@@ -0,0 +1,13 @@
+# Your Name
+ # Date
+ # Assignment Name
+ # A brief description of the project
+
+
+print("Enter your first name: Adam")
+first_name = ("Adam")
+print("Enter your last name: Jones")
+last_name = ("Jones")
+print("Hello Adam Jones! Welcome to CTI-110!")
+
+
