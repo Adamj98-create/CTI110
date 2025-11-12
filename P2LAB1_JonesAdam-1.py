8848a03259047c3ea2c05f626c64deda284b11ab
Adamj98-create <jonesa9837@student.faytechcc.edu>
2025-11-12 17:35:40 -0500

    Add files via upload

---
+++ P2LAB1_JonesAdam-1.py
@@ -0,0 +1,17 @@
+# Adam Jones
+# 12 NOV 2025
+# P2LAB1 - Circle Calculations
+# Assignment Description: This program calculates the diameter, circumference, and area of a circle based on user-inputted radius.
+
+radius = float(input("What is the radius of the circle: "))
+diameter = 2 * radius
+circumference = 2 * 3.14 * radius
+area = 3.14 * radius * radius
+
+
+print("The diameter of the circle is:", diameter)
+print("The circumference of the circle is:", circumference*.02)
+print("The area of the circle is:", area*.03)
+
+
+
